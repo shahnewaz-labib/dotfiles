ad5348f70bc125978f1ae8bc31c57c27b75bd2f2
shahnewaz-labib <shahnewaz@iut-dhaka.edu>
2022-02-07 17:56:35 +0600

    added i3 configs

---
+++ .config/i3/autostart.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Check monitor config
+if [[ `xrandr | grep -w connected | wc -l | grep 1` ]]
+then
+    # only laptop screen connected
+    xrandr --output eDP-1 --auto
+else
+    xrandr --output eDP-1 --off
+    xrandr --output HDMI-1 --auto
+fi
+
+# redshift
+rdshift
+
