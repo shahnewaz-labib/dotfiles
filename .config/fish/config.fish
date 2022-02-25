bc4af7819a3a199efeaa6c3f74293476a605f932
shahnewaz-labib <shahnewaz@iut-dhaka.edu>
2022-02-25 15:54:33 +0600

    qtile something

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+
+alias fn="echo 0 | sudo tee /sys/module/hid_apple/parameters/fnmode"
+alias rdshift="redshift -l 24.1:90.41 & disown"
 alias update='sudo pacman -Syu'
 
 alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
@@ -9,3 +12,6 @@ alias nvimcfg='nvim ~/.config/nvim/init.vim'
 alias g='git'
 alias l='ls -l'
 
+pfetch
+
+set fish_greeting
